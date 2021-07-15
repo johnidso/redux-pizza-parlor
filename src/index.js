5f759e66df7ff172acb356af9f5046d00889f905
Kashia Moua <kserenitym@gmail.com>
2021-07-15 16:41:33 -0500

    Added imports from react

--- src/index.js
+++ src/index.js
@@ -2,8 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import logger from 'redux-logger';
+import {Provider} from 'react-redux';
 
+const totalReducer = (state=0, action) => {
+    if (action.type === "UPDATE_TOTAL"){
+        state += Number(action.payload);
+        return state
+    }
+    return state
+}
 
+let storeInstance= createStore(
+    combineReducers({
+        totalReducer,
+    }),
+    applyMiddleware(logger)
+)
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={storeInstance}> <App />  </Provider>, document.getElementById('root'));
