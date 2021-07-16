0cc0a06a827dc45918ec1e5751d6c22ee506d94f
Kashia Moua <kserenitym@gmail.com>
2021-07-15 22:04:09 -0500

    Finished setting up table for orders on admin file

---
+++ src/components/Admin/Admin.jsx
@@ -0,0 +1,40 @@
+function Admin() {
+
+    return(
+        <table>
+            <thead>
+            <tr>
+                <th>
+                    Customer Name
+                </th>
+                <th>
+                    Time Order was Placed
+                </th>
+                <th>
+                    Type
+                </th>
+                <th>
+                    Total Cost
+                </th>
+                </tr>
+                </thead>
+                <tbody>
+                    {orderList.map((pizzaOrder, index) =>{
+                        return (
+                            <tr key={pizzaOrder.Id}>
+                                <td> {pizzaOrder.time }</td>
+                                <td> {pizzaOrder.customer_name}</td>
+                                <td> {pizzaOrder.street_address}</td>
+                                <td> {pizzaOrder.city}</td>
+                                <td> {pizzaOrder.type}</td>
+                                <td> {pizzaOrder.total}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+        </table>
+    )
+}
+
+
+export default Admin;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 import Header from "../Header/Header";
+import Admin from "../Admin/Admin";
+
+const dispatch = useDispatch();
 
 function App() {
+  state = {
+    pizzaList: [],
+    orderList: [],
+  }
 
+  useEffect(() => {
+    getOrders();
+  }, [])
+  //GET ORDERS REQUEST
+  const getOrders = () => {
+    axios.get('/Orders')
+    .then((response) =>{
+      console.log('AXIOS GET ORDERS response:', response)
+      dispatch({ type: `SET_ORDER_LIST`, payload: response.data });
+      })
+      .catch( error => {
+        console.log(error);
+        alert(`Could not get orders at this time. Try again later.`);
+      })
+  }
+  
   return (
     <>
     <Header />
