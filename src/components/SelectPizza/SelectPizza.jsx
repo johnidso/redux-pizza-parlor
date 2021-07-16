eb53737dd7223e55d604f8550625723612a5cbc7
John Idso <john.d.idso@gmail.com>
2021-07-16 09:52:39 -0500

    added routes fully and tested add/remove conditional rendering.

--- src/components/SelectPizza/SelectPizza.jsx
+++ src/components/SelectPizza/SelectPizza.jsx
@@ -1,16 +1,25 @@
 import {useSelector} from 'react-redux';
 import PizzaItem from '../PizzaItem/PizzaItem';
+import {useHistory} from 'react-router-dom'
 
 
 function SelectPizza () {
+    const history = useHistory()
     const pizzas = useSelector(store => store.pizzas);
     console.log(pizzas);
+    const handleClick= () => {
+        console.log('Routing to info page');
+        history.push('/info');
+    }
     return(
-        <section id='pizzaList'>
-        {pizzas.map( pizza => {
-            return <PizzaItem key={pizza.id} name={pizza.name} description={pizza.description} price={pizza.price} image_path={pizza.image_path} />
-})}
-        </section>
+        <>
+            <section id='pizzaList'>
+            {pizzas.map( pizza => {
+                return <PizzaItem key={pizza.id} name={pizza.name} description={pizza.description} price={pizza.price} image_path={pizza.image_path} />
+            })}
+            </section>
+            <button onClick={handleClick}>Next</button>
+        </>
     )
 }
 
