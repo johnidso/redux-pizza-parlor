00eab0ec1d15009716a49ea2883379297d25810e
Kashia Moua <kserenitym@gmail.com>
2021-07-16 10:55:42 -0500

    updated app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,49 +1,52 @@
 import React, { useEffect } from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import { HashRouter as Router, Route } from "react-router-dom";
 import axios from 'axios';
 import './App.css';
 import Header from "../Header/Header";
-import { useDispatch, useSelector } from 'react-redux';
-import Admin from "../Admin/Admin";
-import { HashRouter as Router, Route, Link} from 'react-router-dom';
-
-
-
+import SelectPizza from '../SelectPizza/SelectPizza';
 
 function App() {
-  
   const dispatch = useDispatch();
-
-  const pizzaOrders = useSelector (store => store.pizzaOrders);
+  const pizzas = useSelector(store => store.pizzas);
 
   useEffect(() => {
+    fetchPizzaList();
     getOrders();
-  }, [])
-  //GET ORDERS REQUEST
-  const getOrders = () => {
-    axios.get('/api/order')
-    .then((response) =>{
-      console.log('AXIOS GET ORDERS response:', response)
-      dispatch({ type: `GET_ORDER_LIST`, payload: response.data });
-      })
-      .catch( error => {
-        console.log(error);
-        alert(`Could not get orders at this time. Try again later.`, error);
-      })
+  }, []);
+
+  const fetchPizzaList = () => {
+    axios.get('/api/pizza')
+    .then( res => {
+        dispatch({type:'GET_PIZZAS', payload: res.data})
+    })
+    .catch( err => {
+        console.log('Error getting Pizzas', err);
+    })
   }
-  
+
+
   return (
-    <>
-  
-      <Header />
-      <img src='images/pizza_photo.png' />
-      <p>Pizza is great.</p>
-      {/* <Route exact path= '/admin' component={Admin}> </Route> */}
-      <Admin />
-    
-    
-    
+    <Router>
+      <Route path='/'>
+        <Header />
+        <SelectPizza />
+      </Route>
+      <Route path='/info'>
+
+      </Route>
+      <Route path='/checkout'>
+
+      </Route>
+      <Route path='/admin'>
+        
+      </Route>
+
       
-    </>
+        <img src='images/pizza_photo.png' />
+        <p>Pizza is great.</p>
+      
+    </Router>
   );
 }
 
